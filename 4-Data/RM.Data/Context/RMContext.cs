d5da6348d5353a2c20398318001f21c9f360a60f
Rodrigo Moura <rodrigomoura@RMoura.local>
2019-01-10 19:24:53 -0200

    Configurando a connection string na classe context

--- 4-Data/RM.Data/Context/RMContext.cs
+++ 4-Data/RM.Data/Context/RMContext.cs
@@ -1,4 +1,6 @@
+using System.IO;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using RM.Domain.Models;
 
 namespace RM.Data.Context
@@ -7,16 +9,17 @@ namespace RM.Data.Context
     {
         public DbSet<Customer> Customer { get; set; }
         public DbSet<Address> Address { get; set; }
-
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
-
         }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            
+            var config = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsetting.json")
+            .Build();
+            optionsBuilder.UseSqlServer(config.GetConnectionString("DefaultConnection"));
         }
-
     }
 }
